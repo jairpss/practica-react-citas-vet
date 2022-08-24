ea337fa8583843c9489a89938f19d96e2c28d6bd
Jair Partida <jairpartida1@gmail.com>
2022-08-23 23:05:06 -0600

    adding tailwind css and styling the header

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'] 
+      },
+    },
+  },
+  plugins: [],
+}
