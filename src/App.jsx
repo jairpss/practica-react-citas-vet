2d314c6b96c61a163955e791c519696568195516
Jair Partida <jairpartida1@gmail.com>
2022-08-23 21:49:58 -0600

    creating our firsts components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+import ListadoPacientes from './components/ListadoPacientes'
 
 function App() {
 
   return (
     <div className="App">
-        <h1>Hola mundo</h1>       
+        <Header />
+        <Formulario />
+        <ListadoPacientes />       
     </div>
   )
 }
